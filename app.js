57ee7136255177e1519963a9ca3cbce36626b995
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-06-10 11:27:04 -0500

    setTimeout for hero

---
+++ app.js
@@ -0,0 +1,16 @@
+let rossJumbo = document.querySelector('.ross-jumbo-img');
+
+window.onload = function(){
+    setTimeout(function(){
+      document.querySelector('.excellence').style.display = 'inline-block'
+    },1000)
+
+    setTimeout(function(){
+      document.querySelector('.creativity').style.display = 'inline-block'
+    },2000)
+
+    setTimeout(function(){
+      document.querySelector('.challenges').style.display = 'inline-block'
+    },3000)
+
+};
