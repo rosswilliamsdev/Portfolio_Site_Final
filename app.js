de9db44599f7133c345ef79191674bbb4cece5fb
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-06-16 18:15:55 -0500

    resume modal attempts

--- app.js
+++ app.js
@@ -17,3 +17,22 @@ window.onload = function(){
       document.querySelector('.arrow').style.display = 'block'
     },4000)
 };
+
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let resumebtn = document.querySelector('.resume-btn');
+let modalImg = document.getElementById("resumepng");
+resumebtn.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
