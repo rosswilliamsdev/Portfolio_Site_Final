be86558f1096e17749c046b5c40e695e9c8ca4ce
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-06-27 14:26:41 -0500

    email submit progress

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-window.onload = function(){
-    setTimeout(function(){
-      document.querySelector('.excellence').style.display = 'block'
-    },1000)
+window.onload = function () {
+  setTimeout(function () {
+    document.querySelector(".excellence").style.display = "block";
+  }, 1000);
 
-    setTimeout(function(){
-      document.querySelector('.creativity').style.display = 'block'
-    },2000)
+  setTimeout(function () {
+    document.querySelector(".creativity").style.display = "block";
+  }, 2000);
 
-    setTimeout(function(){
-      document.querySelector('.challenges').style.display = 'block'
-    },3000)
+  setTimeout(function () {
+    document.querySelector(".challenges").style.display = "block";
+  }, 3000);
 
-    setTimeout(function(){
-      document.querySelector('.arrow').style.display = 'block'
-    },4000)
+  setTimeout(function () {
+    document.querySelector(".arrow").style.display = "block";
+  }, 4000);
 };
 
 ///////////////////////////////
@@ -22,42 +22,74 @@ window.onload = function(){
 let modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal
-let resumebtn = document.querySelector('.resume-btn');
+let resumebtn = document.querySelector(".resume-btn");
 let modalImg = document.getElementById("resumepng");
-resumebtn.onclick = function(){
+resumebtn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 ///////////////////////////////
 // CONTACT FORM ALERT MSGS
 ///////////////////////////////
 
-let sendButton = document.querySelector('.contact-btn');
-let formName = document.querySelector('.name');
-let formEmail = document.querySelector('.email');
-let formSubject = document.querySelector('.subject');
-let formTextbox = document.querySelector('.textbox');
+let sendButton = document.querySelector(".contact-btn");
+let formName = document.querySelector(".name");
+let formEmail = document.querySelector(".email");
+let formSubject = document.querySelector(".subject");
+let formTextbox = document.querySelector(".textbox");
 
-sendButton.addEventListener('click', () => {
-    // All fields are required to send messages
-    
-    formName.required = true;
-    formEmail.required = true;
-    formSubject.required = true;
-    formTextbox.required = true;
+sendButton.addEventListener("click", () => {
+  // All fields are required to send messages
 
-    // If any are blank show an alert, otherwise send the message
-    if (formName.value === '' || formEmail.value === '' || formSubject === '' || formTextbox === '') {
-        alert("Please complete all form fields to send a message.");
-    } else {
-        alert("Message sent. Thanks for reaching out!");
-    }
+  formName.required = true;
+  formEmail.required = true;
+  formSubject.required = true;
+  formTextbox.required = true;
+
+
+  let ebody = `
+  <b>Name: </b>${formName.value}
+  <br>
+  <b>Email: </b>${formEmail.value}
+  <br>
+  <b>Subject: </b>${formSubject.value}
+  <br>
+  <b>Text: </b>${formTextbox.value}
+  <br>
+  `
+
+  // If any are blank show an alert, otherwise send the message
+  if (
+    formName.value === "" ||
+    formEmail.value === "" ||
+    formSubject === "" ||
+    formTextbox === ""
+  ) {
+    alert("Please complete all form fields to send a message.");
+  } else {
+    alert("Message sent. Thanks for reaching out!");
+
+    Email.send({
+      SecureToken: "C973D7AD-F097-4B95-91F4-40ABC5567812",
+      To: "rossw.dev@gmail.com",
+      From: "rossw.dev@gmail.com",
+      Subject: "This is the subject",
+      Body: ebody,
+    }).then((message) => alert(message));
+
+  }
 });
+
+///////////////////////////////
+// SENDING EMAILS FROM MESSAGE FORM
+///////////////////////////////
+
+
